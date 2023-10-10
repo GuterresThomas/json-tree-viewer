3f9fa1b03c201b9ef04deab8245f351fedd8f51b
Guterresthomas <guterresmikael@gmail.com>
2023-10-10 18:51:51 -0300

    json-tree-viewer

--- src/components/jsonviewer.vue
+++ src/components/jsonviewer.vue
@@ -1,9 +1,53 @@
 <template>
-    <div>
-      <input type="file" @change="loadJson" />
-      <pre>{{ jsonTree }}</pre>
-    </div>
-  </template>
+  <div>
+    <label class="custom-input-label">
+      <input
+        class="custom-input"
+        type="file"
+        @change="loadJson"
+      />
+      Escolher arquivo JSON
+    </label>
+    <div v-if="jsonTree" class="file-name"> {{ fileName }}</div>
+    <pre>{{ jsonTree }}</pre>
+  </div>
+</template>
+
+  <style>
+
+    .custom-input-label {
+    position: relative;
+    cursor: pointer;
+    display: inline-block;
+    padding: 0.5rem 1rem;
+    background-color: rgb(37, 146, 9);
+    color: #fff;
+    border-radius: 0.25rem;
+    }
+
+    .custom-input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    }   
+
+
+    .file-name {
+        font-weight: 700;
+        margin: 1rem;
+        font-size: larger;
+    }
+
+
+    pre {
+        font-weight: 400;
+        color: rgb(37, 146, 9);
+    }
+  </style>
   
   <script>
   export default {
@@ -16,6 +60,7 @@
       loadJson(event) {
         const file = event.target.files[0];
         if (file) {
+          this.fileName = file.name;
           const reader = new FileReader();
           reader.onload = (e) => {
             try {
